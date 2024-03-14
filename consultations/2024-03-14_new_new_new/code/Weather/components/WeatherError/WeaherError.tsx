33bfbc8bccc86e956fab3828157dc80de6b0f8c9
Vladimir_Leonov <2227331.spb@gmail.com>
2024-03-14 23:26:47 +0300

    paths_at_2024_03_14

---
+++ consultations/2024-03-14_new_new_new/code/Weather/components/WeatherError/WeaherError.tsx
@@ -0,0 +1,8 @@
+// В компоненте WeatherError должны быть показаны данные о погоде
+// Данные должны приходить через props от комопонента Weather
+
+function WeatherError() {
+  return <h1>Ошибка</h1>;
+}
+
+export default WeatherError;
